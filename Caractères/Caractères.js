3c7d2db5c9c19cb219ca1b27282c994f5b0ccbb7
Artus de Chavagnac <artus2003chavagnac@gmail.com>
2024-07-21 19:11:46 +0200

    maj

--- Caractères/Caractères.js
+++ Caractères/Caractères.js
@@ -5,8 +5,8 @@
 // ================================================================================================================= //
 
 copier = (caractère) => {
-	navigator.clipboard.writeText(caractère).then(
-		() => {alert("Caractère « " + caractère + " » copié !");}
+	navigator.clipboard.writeText(caractère.innerHTML).then(
+		() => {alert("Caractère « " + caractère.innerHTML + " » copié !");}
 	);
 };
 document.addEventListener(
@@ -14,45 +14,43 @@ document.addEventListener(
 	function () {
 		if (document.title == "Alt +") {
 			éléments = document.querySelectorAll("td");
-		} else if (document.title == "Entités HTML") {
+		} else {
 			éléments = document.querySelectorAll("tr");
-		} else if (document.title == "Renversés") {
-			éléments = document.querySelectorAll("th");
 		}
 		total = éléments.length;
 		occurrences = {};
-		classes = 0;
+		nombreDeClasses = 0;
 		éléments.forEach(
 			function (élément) {
 				classe = élément.getAttribute("class");
 				if (classe) {
-					if (!occurrences[classe]) {
-						occurrences[classe] = 1;
-					} else {
-						occurrences[classe]++;
-					}
-					classes++;
+					if (!occurrences[classe]) {occurrences[classe] = 1;} else {occurrences[classe]++;}
+					nombreDeClasses++;
 				}
 			}
 		);
 		tableStatistiques = document.getElementById("table-statistiques");
-		premièreLigneStatistiques();
-		nouvelleLigneStatistiques("Défaut", total - classes, total);
-		for (classe in occurrences) {
-			nouvelleLigneStatistiques(classe, occurrences[classe], total);
-		}
+		tableStatistiques.insertRow().insertCell(0).outerHTML = "<th>Classe</th><th>Occurrences</th><th>Proportion</th>";
+		nouvelleLigneStatistiques("Défaut", total - nombreDeClasses, total);
+		for (classe in occurrences) {nouvelleLigneStatistiques(classe, occurrences[classe], total);}
 		nouvelleLigneStatistiques("Total", total, total);
+		polices.addEventListener(
+			"change", function() {
+				police = polices.value;
+				for (var i = document.querySelectorAll("#table-caractères td").length - 1; i >= 0; i--) {
+					document.querySelectorAll("#table-caractères td")[i].style.fontFamily = police;
+				}
+				input.style.fontFamily = police;
+			}
+		);
 	}
 );
 function nouvelleLigneStatistiques(classe, occurrences, total) {
 	pourcentage = (occurrences / total) * 100;
 	ligne = tableStatistiques.insertRow();
-	ligne.insertCell(0).textContent = classe;
+	ligne.insertCell(0).outerHTML = "<td class = '" + classe + "'>" + classe + "</td>";
 	ligne.insertCell(1).textContent = occurrences;
 	ligne.insertCell(2).textContent = pourcentage.toFixed(1).replace(".", ",") + " %";
-}
-function premièreLigneStatistiques() {
-	tableStatistiques.insertRow().insertCell(0).outerHTML = "<th>Classe</th><th>Occurrences</th><th>Pourcentage</th>";
 }
 
 // ================================================================================================================= //
