b1636bcbcf462d75d046f3ffb999b859afc03a19
Le Cavalier Riant <130751738+lecavalierriant@users.noreply.github.com>
2025-01-07 23:04:43 +0100

    Update js.js

--- Web/js.js
+++ Web/js.js
@@ -2,24 +2,16 @@
 //                                                                                                                   //
 // ================================================================================================================= //
 
-
 var heure = new Date().getHours();
 
-// ================================================================================================================= //
-//                                                                                                                   //
-// ================================================================================================================= //
-
 function page() {
-
 	"use strict";
-	document.writeln("<!doctype html><head><title>HTML</title><link rel = stylesheet href = css.css><script src = js.js></script></head><div class = divAnimation1>divAnimation1</div><div class = divAnimation1>divAnimation1</div><button onclick = messageVariable()>messageVariable()</button><button onclick = sourceVariable()>sourceVariable()</button><form action = html.html method = get><input type = password name = motDePasse> password<br><br><input type = radio name = entreeRadio checked><input type = radio name = entreeRadio> radio<br><br><input type = checkbox name = caseACocher checked><input type = checkbox name = caseACocher> checkbox<br><br><input type = color name = couleur> color<br><br><input type = file name = fichier> file<br><br><input type = number name = nombre> number<br><br><input type = range name = potentiometre> range<br><br><input type = time name = heure> time<br><br><input type = datetime-local name = dateComplete> datetime-local<br><br><input type = week name = semaine> week<br><br><input type = month name = mois> month<br><br><input type = url name = entreeUrl> url</form>");
-
+	document.writeln("<p>Nouvelle page</p>");
 }
 
 var message = "";
 
 function messageVariable() {
-
 	if (heure >= 6 && heure < 8) {message = "<h1>Bonne journée !</h1>";}
 	else if (heure >= 8 && heure < 12) {message = "<h1>Elle commence bien cette journée ?</h1>";}
 	else if (heure >= 12 && heure < 13) {message = "<h1>Il faut aller manger !</h1>";}
@@ -28,13 +20,11 @@ function messageVariable() {
 	else {message = "<h1>Vous ne dormez jamais?!</h1>";}
 	document.writeln(message);
 	page();
-
 }
 
 var source = "";
 
 function sourceVariable() {
-
 	if (heure >= 6 && heure < 8) {source = "image1.png";}
 	else if (heure >= 8 && heure < 12) {source = "image1.png";}
 	else if (heure >= 12 && heure < 13) {source = "image2.png";}
@@ -43,12 +33,16 @@ function sourceVariable() {
 	else {source = "image3.png";}
 	document.writeln("<img src = " + source +" alt = " + source + ">");
 	page();
-
 }
 
 // confirm("Voulez-vous ?");
 // sorte d'alert() avec un bouton « Annuler » et un autre « OK »
 
+// Fontions des listes
+
+liste = [0, 0, 1, 1, 2, 2, 2, 3, 4, 5, 5, 6, 7, 8, 9];
+
+console.log(liste);
 
 // ================================================================================================================= //
 //                                                                                                                   //
